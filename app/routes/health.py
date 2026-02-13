5b7475f2f778860fb1095ed9f455945eaad95244
vaisakhp07 <pvaisakh07@gmail.com>
2026-02-13 10:03:38 +0000

    NACL 80 enabled

--- app/routes/health.py
+++ app/routes/health.py
@@ -4,4 +4,4 @@ health_bp = Blueprint("health", __name__)
 
 @health_bp.route("/", methods=["GET"])
 def health():
-    return jsonify({"status": "Done"}), 200
+    return jsonify({"status": "DONEEEEEE"}), 200
